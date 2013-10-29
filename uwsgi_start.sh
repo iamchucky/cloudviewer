61025b2f850ddb1eecaabe501853992726b56646
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-29 15:35:49 -0400

    Can't get it to work consistently with the unix socket.  I'll leave it this way for now.

--- uwsgi_start.sh
+++ uwsgi_start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-touch /tmp/cloudviewer.sock
-chmod www-data:www-data /tmp/cloudviewer.sock
-uwsgi --socket /tmp/cloudview.sock --module pointcloud --callab app --async 64 --ugreen
+uwsgi --socket 127.0.0.1:4242 --module pointcloud --callab app --async 64 --ugreen  
