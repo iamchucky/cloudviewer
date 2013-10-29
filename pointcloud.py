9f3405b04e8017c063be738bcefb89c8c2ac0c02
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-29 13:53:01 -0400

    If we ever decide to switch away from sqlite to something with a similar API, such as sqlalchemy, then we can't assume that a relation's attributes have any implicit order (is that assumption always valid in sqlite?).  Here, I removed the asterisk from the query strings and replaced them with explicit attribute names.  Also, I'm unsure if connections would be quickly closed if we forget to close them, so I put that line inside a finally block to make sure it runs even if we encounter an unhandled exception.

--- pointcloud.py
+++ pointcloud.py
@@ -18,11 +18,8 @@ def pointToJson(pt):
 		jsonPt[fields[i]] = val 
 	return jsonPt
 
-def rowsToBytes(rows, indices):
-	data = []
-	for row in rows:
-		for index in indices:
-			data.append(row[index])
+def rowsToBytes(rows):
+	data = [col for cols in rows for col in cols]
 	bytes = array.array('f', data)
 	return bytes.tostring()
 
@@ -31,32 +28,42 @@ def getPt():
 	start = request.args.get('start', 0, type=int)
 	num = request.args.get('num', 20, type=int)
 	conn = sqlite3.connect('times-square.db')
-	c = conn.cursor()
-	rows = c.execute('select * from points limit '+str(start)+','+str(num))
-	bytes = rowsToBytes(rows, [1,2,3])
-	conn.close()
-	return Response(bytes, mimetype='application/octet-stream')
+
+	try:
+		c = conn.cursor()
+		rows = c.execute('select x,y,z from points limit '+str(start)+','+str(num))
+		bytes = rowsToBytes(rows)
+		return Response(bytes, mimetype='application/octet-stream')
+	finally:
+		conn.close()
 
 @app.route('/api/getPtColors')
 def getPtColors():
 	start = request.args.get('start', 0, type=int)
 	num = request.args.get('num', 20, type=int)
 	conn = sqlite3.connect('times-square.db')
-	c = conn.cursor()
-	rows = c.execute('select * from points limit '+str(start)+','+str(num))
-	bytes = rowsToBytes(rows, [3,4,5])
-	conn.close()
-	return Response(bytes, mimetype='text/xml')
+
+	try:
+		c = conn.cursor()
+		rows = c.execute('select r,g,b from points limit '+str(start)+','+str(num))
+		bytes = rowsToBytes(rows)
+		return Response(bytes, mimetype='application/octet-stream')
+	finally:
+		conn.close()
 
 @app.route('/api/getPt.json')
 def getPtJson():
 	start = request.args.get('start', 0, type=int)
 	num = request.args.get('num', 20, type=int)
 	conn = sqlite3.connect('times-square.db')
-	c = conn.cursor()
-	rows = c.execute('select * from points limit '+str(start)+','+str(num))
-	pts = [pointToJson(row) for row in rows]
-	return jsonify({'points':pts})
+
+	try:
+		c = conn.cursor()
+		rows = c.execute('select '+','.join(fields)+' from points limit '+str(start)+','+str(num))
+		pts = [pointToJson(row) for row in rows]
+		return jsonify({'points':pts})
+	finally:
+		conn.close()
 
 @app.route('/')
 def index():
